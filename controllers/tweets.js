d073d5c14576685d69f4cc6489aa6c6e569a0120
Amir Haghighati <haghighati.amir@gmail.com>
2019-02-20 14:40:35 -0500

    update controller

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,12 +1,15 @@
 const { Controller } = require('bak')
-const { Tweet } = require('../models')
+const Twit = require('twit')
+const config = require('config')
+const { Tweet, Request } = require('../models')
 
 export default class APIController extends Controller {
     init () {
+        this.$twit = new Twit(config.get('twit'))
         this.get('/tweet/{id}', this.getTweet)
         this.get('/tweets', this.getTweets)
+        this.post('/tweets/update', this.updateTweets)
         this.get('/keywords', this.getKeywords)
-
     }
 
     getTweet (request, h) {
@@ -18,6 +21,24 @@ export default class APIController extends Controller {
         try {
             let tweets = await Tweet.find()
             return {data: tweets}
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async updateTweets (request, h) {
+        try {
+            let resp = await this.$twit.get('search/tweets', { q: request.payload.q, count: 200000 })
+                .then(async function (result) {
+                    let req = new Request(result.data.search_metadata)
+                    req.save()
+                    for (let tw of result.data.statuses) {
+                        let tweet = new Tweet(tw)
+                        tweet.save()
+                    }
+                    return result.data.search_metadata
+                })
+            return {data: resp}
         } catch (e) {
             console.log(e)
         }
--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var config = require('config')
 
 var T = new Twit(config.get('twit'))
 
-T.get('search/tweets', { q: 'کامفورت زون', count: 1000 }, function(err, data, response) {
-    console.log(data.statuses)
+T.get('search/tweets', { q: 'HIV since:2006-01-01', count: 200000 }, function(err, data, response) {
+    console.log(data.statuses[0])
 })
