b9e368c2e8a5c6b0c6c87dc4833d56a5c10a1e5c
Amir Haghighati <haghighati.amir@gmail.com>
2019-02-20 14:39:10 -0500

    update Tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -5,7 +5,29 @@ class Tweet extends Model {
     static get $schema () {
         return {
             id: String,
+            id_str: String,
             text: String,
+            truncated: Boolean,
+            entities: Object,
+            metadata: Object,
+            source: String,
+            in_reply_to_status_id: String,
+            in_reply_to_status_id_str: String,
+            in_reply_to_user_id: String,
+            in_reply_to_user_id_str: String,
+            in_reply_to_screen_name: String,
+            user: Object,
+            geo: Object,
+            coordinates: Object,
+            place: Object,
+            contributors: Object,
+            retweeted_status: Object,
+            is_quote_status: Boolean,
+            retweet_count: Number,
+            favorite_count: Number,
+            favorited: Boolean,
+            retweeted: Boolean,
+            lan: String,
         }
     }
 }
