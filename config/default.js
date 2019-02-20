d006315fb8dfacc23cbe0c85a616219ae13a1da1
Amir Haghighati <haghighati.amir@gmail.com>
2019-02-20 08:04:36 -0500

    update default files

---
+++ config/default.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mongo: {
+    connections: {
+      default: {
+        uri: 'localhost'
+      }
+    }
+  },
+  twit: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+  }
+}
--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var config = require('config')
 var T = new Twit(config.get('twit'))
 
 T.get('search/tweets', { q: 'کامفورت زون', count: 1000 }, function(err, data, response) {
-    console.log(data.statuses[0].user)
+    console.log(data.statuses)
 })
