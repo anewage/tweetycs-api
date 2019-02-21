d984e93afaaa07d51db10be185418b3d3d38e6c7
Amir Haghighati <haghighati.amir@gmail.com>
2019-02-21 00:56:43 -0500

    update TweetsController

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,62 +1,30 @@
 const { Controller } = require('bak')
-const Twit = require('twit')
 const Boom = require('boom')
-const config = require('config')
-const { Tweet, Request } = require('../models')
+const { DerivedTweet } = require('../models')
 
-export default class APIController extends Controller {
+export default class TweetsController extends Controller {
+
+    /**
+     * Includes the routings for this controller
+     */
     init () {
-        this.$twit = new Twit(config.get('twit'))
-        this.get('/tweet/{id}', this.getTweet)
         this.get('/tweets', this.getTweets)
-        this.post('/tweets/update', this.updateTweets)
-        this.get('/keywords', this.getKeywords)
     }
 
-    async getTweet (request, h) {
-        try {
-            let tweet = await Tweet.findOne({ 'id': request.params.id })
-            if (tweet) {
-                return {data: tweet}
-            } else {
-                return Boom.notFound()
-            }
-        } catch (e) {
-            console.log(e)
-            throw Boom.badRequest(e)
-        }
-    }
 
+    /**
+     * Get batch of tweets stored in the database based on the query
+     * @param request
+     * @param h
+     * @returns {Promise<{data: *}>}
+     */
     async getTweets (request, h) {
         try {
-            let tweets = await Tweet.find()
+            let tweets = await DerivedTweet.find(request.query)
             return {data: tweets}
         } catch (e) {
             console.log(e)
             throw Boom.badRequest(e)
         }
-    }
-
-    async updateTweets (request, h) {
-        try {
-            let resp = await this.$twit.get('search/tweets', { q: request.payload.q, count: 200000 })
-                .then(async function (result) {
-                    let req = new Request(result.data.search_metadata)
-                    req.save()
-                    for (let tw of result.data.statuses) {
-                        let tweet = new Tweet(tw)
-                        tweet.save()
-                    }
-                    return result.data.search_metadata
-                })
-            return {data: resp}
-        } catch (e) {
-            console.log(e)
-            Boom.badRequest(e)
-        }
-    }
-
-    getKeywords (request, h) {
-        return {data: 'Keywords...'}
     }
 }
