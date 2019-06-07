818a7d909c66b669940891456e7ee534696e4045
verinachen <verinachen@outlook.com>
2019-06-06 22:52:35 -0400

    Update index.js

--- modules/mldrivers/index.js
+++ modules/mldrivers/index.js
@@ -1,7 +1,17 @@
-const SampleMLAdapter = require('./sample')
+const LSTMAdapter = require('./lstm')
+const CNNAdapter = require('./cnn')
+const SVMAdapter = require('./svm')
+const KNNAdapter = require('./knn')
+const RFAdapter = require('./rf')
+const MLPAdapter = require('./mlp')
 
 let adapters = []
-adapters.push(new SampleMLAdapter('MY Great Sample machine Learning Adapter', 'mladapter1'))
+adapters.push(new LSTMAdapter('MY Great Sample machine Learning Adapter', 'mladapter1'))
+adapters.push(new CNNdapter('MY Great Sample machine Learning Adapter', 'mladapter2'))
+adapters.push(new SVMdapter('MY Great Sample machine Learning Adapter', 'mladapter3'))
+adapters.push(new KNNdapter('MY Great Sample machine Learning Adapter', 'mladapter4'))
+adapters.push(new RFdapter('MY Great Sample machine Learning Adapter', 'mladapter5'))
+adapters.push(new MLPdapter('MY Great Sample machine Learning Adapter', 'mladapter6'))
 
 async function analyzeTweet(tweet) {
     let res = []
@@ -17,3 +27,4 @@ module.exports = {
         return await analyzeTweet(tweet)
     }
 }
+
