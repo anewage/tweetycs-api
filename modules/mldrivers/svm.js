fae5a36e4936f274691e173899f5967b06516ff6
verinachen <verinachen@outlook.com>
2019-06-06 22:51:27 -0400

    Add ml drivers

---
+++ modules/mldrivers/svm.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const Driver = require('./driver')
+const url1 = 'http://localhost:33508/svmtweet'
+ const url2 = 'http://localhost:33508/svmuser'
+
+
+class   SVMAdapter extends Driver{
+  async predict(tweet,user) {
+    // Write code to predict or label a tweet based on user, text, etc...
+    let res1 = await axios.post(url1, {
+       'tweet': tweet
+     }).then(function (response1){
+        // response = JSON.parse(response)
+       return response1.data
+     })
+
+    let res2 = await axios.post(url2, {
+       'user_description':user
+     }).then(function (response2){
+       // response2 = JSON.parse(response)
+       return response2.data
+     })
+
+     var res = {
+      "theme":res1["theme"],
+      "group":res2["group"],
+     }
+    return res
+  }
+}
+
+module.exports = SVMAdapter
