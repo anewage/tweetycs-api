974630c2685f14fb5b25e5158b17b00faf26e8ff
Amir Haghighati <haghighati.amir@gmail.com>
2019-06-06 21:23:33 -0400

    update default config

--- config/default.js
+++ config/default.js
@@ -1,9 +1,13 @@
 module.exports = {
+  port: 2000,
+  bakjs: 'http://tweetycs_bak:3000/api/tweet/save',
+  namespace: '/tweetycs',
   twit: {
     consumer_key: '',
     consumer_secret: '',
     access_token: '',
-    access_token_secret: ''
+    access_token_secret: '',
+    tweet_mode: 'extended'
   },
   IBM: {
     api_key: '',
